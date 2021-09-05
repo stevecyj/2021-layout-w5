4967039f3182902c668f717d41de45a481baa37c
root <root@localhost.localdomain>
2021-09-05 18:22:43 +0800

    Update 2021-09-05T10:22:33.775Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,5 +3,11 @@
 console.log('Hello!');
 $(document).ready(function () {
   console.log('HesSchool Hello!');
+}); // BS5 modal js
+
+var myModal = document.getElementById('myModal');
+var myInput = document.getElementById('myInput');
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus();
 });
 //# sourceMappingURL=all.js.map
